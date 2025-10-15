1d27916aac3acd12d288c4457dd5e5e698cfff54
Marina Nobre <mahnobre06@gmail.com>
2025-10-15 11:03:29 -0300

    Add files via upload
    Este programa solicita dois números ao usuário e informa se a sequência é crescente ou decrescente. O processo se repete até que os números sejam iguais.

---
+++ crescente.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+	int x;
+	int y;
+	
+	
+	while( x != y ){
+		
+	printf("digite um primeiro numero: ");
+	scanf("%d", &x);
+	printf("digite um segundo numero: ");
+	scanf("%d", &y);
+		
+		if( x < y ){
+			printf("CRESCENTE\n");
+		} else 
+			printf("DECRESCENTE\n");
+		}
+		
+	printf("digite um primeiro numero: ");
+	scanf("%d", &x);
+	printf("digite um segundo numero: ");
+	scanf("%d", &y);
+	
+	return 0;
+	
+	}
+

214ac34b0985c05eba4e4560bc505a71aac7d775
Marina Nobre <mahnobre06@gmail.com>
2026-03-03 19:00:38 -0300

    Update and rename numerospares.c to C/numerospares.c

--- C/numerospares.c
+++ C/numerospares.c
@@ -1,4 +1,4 @@
-// Faça um programa que leia N números inteiros e armazene-os em um vetor. Em seguida, mostre na tela todos os números pares, e também a quantidade de números pares.
+// FaÃ§a um programa que leia N nÃºmeros inteiros e armazene-os em um vetor. Em seguida, mostre na tela todos os nÃºmeros pares, e tambÃ©m a quantidade de nÃºmeros pares.
 
 #include<stdio.h>
 
